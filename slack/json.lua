658532460348d62bce42f5605b213b0fda5c8ba7
ConnorVG <ConnorVG@gmx.com>
2014-03-23 12:55:50 +0000

    Fixed the broken JSON encoder
    Because, well, it was broke.
    
    Passing any " ' " would invalidate the JSON.

--- slack/json.lua
+++ slack/json.lua
@@ -330,7 +330,7 @@ end
 function encodeString(s)
 	s = string.gsub(s,'\\','\\\\')
 	s = string.gsub(s,'"','\\"')
-	s = string.gsub(s,"'","\\'")
+--	s = string.gsub(s,"'","\\'")
 	s = string.gsub(s,'\n','\\n')
 	s = string.gsub(s,'\t','\\t')
 	return url_encode(s) 
